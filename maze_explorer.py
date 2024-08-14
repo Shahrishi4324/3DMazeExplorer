890c40079d56a295586a983c12294c1093cc6f6e
Rishi Shah <smitjohn3221@gmail.com>
2024-08-14 12:57:38 -0400

    Add files via upload

--- maze_explorer.py
+++ maze_explorer.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import sys
+import numpy as np
 
 # Initialize Pygame
 pygame.init()
@@ -67,9 +68,18 @@ def setup_camera():
     glLoadIdentity()
     gluLookAt(0, 1, -10, 0, 1, 0, 0, 1, 0)
 
+# Collision detection
+def check_collision(pos):
+    x, y, z = pos
+    if x < -10 or x > 10 or z < -10 or z > 10:
+        return True
+    return False
+
 # Main game loop
 def game_loop():
     setup_camera()
+
+    pos = np.array([0.0, 1.0, -5.0])
 
     running = True
     while running:
@@ -79,6 +89,24 @@ def game_loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        # Handle player movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            pos[0] -= 0.1
+        if keys[pygame.K_RIGHT]:
+            pos[0] += 0.1
+        if keys[pygame.K_UP]:
+            pos[2] += 0.1
+        if keys[pygame.K_DOWN]:
+            pos[2] -= 0.1
+
+        if check_collision(pos):
+            pos[0], pos[2] = 0, -5  # Reset to start if collision occurs
+
+        # Update camera position
+        glLoadIdentity()
+        gluLookAt(pos[0], pos[1], pos[2], pos[0], pos[1], pos[2] + 1, 0, 1, 0)
 
         # Render maze
         render_maze()
